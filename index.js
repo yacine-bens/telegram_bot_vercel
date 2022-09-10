5b723792369c8f9e06700d20caf6ffa097f9ff07
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2022-09-10 20:17:45 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const {TOKEN, SERVER_URL} = process.env;
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+const URI = `/webhook/${TOKEN}`;
+const WEBHOOK_URL = SERVER_URL + URI;
+
+const app = express();
+app.use(bodyParser.json());
+
+// Tell Telegram server where to send POST requests
+const init = async () => {
+    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
+    console.log(res.data);
+}
+
+app.post(URI, async (req, res) => {
+    console.log(req.body);
+
+    const chatId = req.body.message.chat.id;
+    const text = req.body.message.text;
+
+    const message = `Your message contains ${wordCount(text)} words.`;
+    
+    await axios.post(`${TELEGRAM_API}/sendMessage`, {
+        chat_id: chatId,
+        text: message
+    })
+    
+    return res.send();
+})
+
+app.listen(process.env.PORT || 5000, async () => {
+    console.log('App is running on port', process.env.PORT || 5000);
+    await init();
+})
+
+function wordCount(str){
+    return str.split(' ').filter(l => l != '').length;
+}
